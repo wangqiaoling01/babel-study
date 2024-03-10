50fe4605c9f1c45973d8b2ade8acede0a7475892
wangqiaoling <1113584801@qq.com>
2024-03-10 12:09:28 +0800

    feat: babel config

--- .history/src/index_20240310114929.js
+++
@@ -1,6 +0,0 @@
-// es6 语法 babel可以解析
-const sum = (a, b) => a + b
-// Promise 是新的api babel默认无法解析 babel值解析语法
-Promise.resolve(100).then(data => data);
-[1,2,3].includes(100)
-
--- .history/src/index_20240310120740.js
+++
@@ -1,7 +0,0 @@
-// es6 语法 babel可以解析
-const sum = (a, b) => a + b
-// Promise 是新的api babel默认无法解析 babel值解析语法
-// import '@babel/polyfill'; 按需引入时不需要写这句话
-Promise.resolve(100).then(data => data);
-[1,2,3].includes(100)
-
